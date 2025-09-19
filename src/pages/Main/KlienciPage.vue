edf9823f8e8049e2301f62bc92fa2e3a3dd298b5
d3c1s1ve <decisive.three@gmail.com>
2025-09-19 15:04:48 +0200

    hero minibanner

--- src/pages/Main/KlienciPage.vue
+++ src/pages/Main/KlienciPage.vue
@@ -11,6 +11,13 @@ import AppCard from '@/components/Global/AppCard.vue'
           alt="Shell V-Power"
           class="hero-img"
         />
+        <div class="mini-banner">
+          <h1>W pełni ciesz się prowadzeniem samochodu</h1>
+          <p>
+            Znajdź inspirację, odkrywaj i dowiedz się, jak uzyskać najlepsze osiągi swojego pojazdu
+            dzięki produktom Shell.
+          </p>
+        </div>
       </div>
       <div class="card-container">
         <AppCard
@@ -36,12 +43,40 @@ import AppCard from '@/components/Global/AppCard.vue'
   display: flex;
   gap: 2rem;
 
-  .hero-img {
-    flex: 8;
-    border-radius: vars.$border-radius;
-    width: 84.4rem;
-    height: 76.5rem;
-    object-fit: cover;
+  .hero-img-container {
+    position: relative;
+
+    .hero-img {
+      flex: 8;
+      border-radius: vars.$border-radius;
+      width: 84.4rem;
+      height: 76.5rem;
+      object-fit: cover;
+    }
+
+    .mini-banner {
+      width: 34rem;
+      height: 43.9rem;
+      border-radius: vars.$border-radius;
+      background-color: rgba(0, 0, 0, 0.5);
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 10%;
+      padding: 2.4rem;
+
+      h1 {
+        font-size: 3.2rem;
+        line-height: 1.5;
+        color: vars.$light-grey;
+        margin-bottom: 2rem;
+      }
+
+      p {
+        font-size: 1.4rem;
+        line-height: 1.5;
+      }
+    }
   }
 
   .card-container {
